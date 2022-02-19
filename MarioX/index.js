566a62116a706c153b270150f5393bd9c02ec07a
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-02-19 13:43:19 -0500

    mariox project setup

---
+++ MarioX/index.js
@@ -0,0 +1,7 @@
+// 1 - Project Setup Start
+// 1.1 select the canvas element in the html doc
+const canvas = document.querySelector('canvas');
+// 1.2 access context
+const context = canvas.getContext('2d');
+console.log(context);
+// 1 - Project Setup End
